993ad4eeac910e8cf01e5e398b8b1465033a0d68
Umara506 <90119285+Umara506@users.noreply.github.com>
2021-09-04 21:33:09 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+
+/************************************************
+ * Hello
+ * **********************************************
+ * Asim Umar
+ * 9-3-2021
+ * 255 and 004
+ * ********************************************/
+
+
+public class Hello {
+ public static void main (String[]args)	{
+  
+  System.out.println("Asim Umar");
+  System.out.println("Hello");
+  System.out.println("printing infromation");
+  System.out.println("09-03-2021");
+  System.out.println("255 and 004");
+  System.out.println();
+
+ }
+
+}
